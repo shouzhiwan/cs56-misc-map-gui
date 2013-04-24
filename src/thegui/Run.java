01e4eaadc1935a3d60b195b62d88cee95ec70f8e
Carina Rammelkamp <c.rammelkamp@gmail.com>
2013-04-24 11:19:46 -0700

    added project files

---
+++ src/thegui/Run.java
@@ -0,0 +1,15 @@
+package thegui;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Aki Stankoski and Dennis Huynh
+ */
+public class Run{
+public static void main(String[] args) throws IOException {
+TheGUI a = new TheGUI();
+a.setUpHomeScreen();
+}
+}
