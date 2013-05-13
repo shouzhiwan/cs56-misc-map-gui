c1e299899f5f3e0581a40b5814b11e7803048e7d
Eric <eric_j_huang@umail.ucsb.edu>
2013-05-12 22:46:05 -0700

    everything should work now

--- src/edu/ucsb/cs56/projects/misc/map_gui/Run.java
+++ src/edu/ucsb/cs56/projects/misc/map_gui/Run.java
@@ -7,6 +7,7 @@ import java.io.IOException;
  *
  * @author Aki Stankoski and Dennis Huynh
  */
+ 
 public class Run{
 public static void main(String[] args) throws IOException {
 TheGUI a = new TheGUI();
